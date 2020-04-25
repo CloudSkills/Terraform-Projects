a435c56672d3663cce920d8696eb535fb32efe57
Michael Levan <mlevan1992@gmail.com>
2020-04-25 13:27:56 -0400

    Terraform code for ACI

---
+++ Containers-And-Orchestration/Containers/Terraform/aci/variables.tf
@@ -0,0 +1,9 @@
+variable "RG" {
+  type = string
+  description = "Name of resource group for the container instance to reside in"
+}
+
+variable "location" {
+  type = string
+  description = "The location/region that you want the container instance to reside in"
+}
