b6bf6a2552f8fcf7385599b0150a0815f7e63e57
Michael Levan <mlevan1992@gmail.com>
2020-05-23 13:03:59 -0400

    Create a vNet

---
+++ 4-Build-Azure-Infrastructure/vNet/terraform.tfvars
@@ -0,0 +1,3 @@
+subscriptionID = "220284d2-6a19-4781-87f8-5c564ec4fec9"
+resourceGroupName = "CloudSkilsRG1992"
+location = "eastus"
---
+++ 4-Build-Azure-Infrastructure/vNet/variables.tf
@@ -0,0 +1,14 @@
+variable "subscriptionID" {
+    type = string
+    description = "Variable for our resource group"
+}
+
+variable "resourceGroupName" {
+    type = string
+    description = "name of resource group"
+}
+
+variable "location" {
+    type = string
+    description = "location of your resource group"
+}
