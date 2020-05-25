5ed0a4daa1e71d7df140770e57294552a088c51d
allanore <orellanaluke@gmail.com>
2020-05-25 14:36:19 -0400

    modified dependancies demo

--- 5-Resource-Dependencies/main.tf
+++ 5-Resource-Dependencies/main.tf
@@ -1,19 +1,24 @@
+locals {
+  system = "CSkillsApp"
+}
+
+
 provider "azurerm" {
   version = "1.38.0"
 }
 
 #create resource group
 resource "azurerm_resource_group" "rg" {
-    name     = "rg-MyFirstTerraform"
+    name     = "rg-${local.system}"
     location = "westus2"
     tags      = {
-      Environment = "Terraform Demo"
+      Environment = local.system
     }
 }
 
 #Create virtual network
 resource "azurerm_virtual_network" "vnet" {
-    name                = "vnet-dev-westus2-001"
+    name                = "vnet-${local.system}-westus2-001"
     address_space       = ["10.0.0.0/16"]
     location            = "westus2"
     resource_group_name = azurerm_resource_group.rg.name
@@ -21,7 +26,7 @@ resource "azurerm_virtual_network" "vnet" {
 
 # Create subnet
 resource "azurerm_subnet" "subnet" {
-  name                 = "snet-dev-westus2-001 "
+  name                 = "snet-${local.system}-westus2-001 "
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefix       = "10.0.0.0/24"
@@ -29,7 +34,7 @@ resource "azurerm_subnet" "subnet" {
 
 # Create public IP
 resource "azurerm_public_ip" "publicip" {
-  name                = "pip-vmterraform-dev-westus2-001"
+  name                = "pip-${local.system}-dev-westus2-001"
   location            = "westus2"
   resource_group_name = azurerm_resource_group.rg.name
   allocation_method   = "Static"
@@ -57,13 +62,13 @@ resource "azurerm_network_security_group" "nsg" {
 
 # Create network interface
 resource "azurerm_network_interface" "nic" {
-  name                      = "nic-01-vmterraform-dev-001 "
+  name                      = "nic-01-${local.system}-dev-001 "
   location                  = "westus2"
   resource_group_name       = azurerm_resource_group.rg.name
   network_security_group_id = azurerm_network_security_group.nsg.id
 
   ip_configuration {
-    name                          = "niccfg-vmterraform"
+    name                          = "niccfg-${local.system}"
     subnet_id                     = azurerm_subnet.subnet.id
     private_ip_address_allocation = "dynamic"
     public_ip_address_id          = azurerm_public_ip.publicip.id
